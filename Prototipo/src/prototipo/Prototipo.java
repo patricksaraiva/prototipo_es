d411f6837013315a5eb5aec89c21e5bc40a20788
patricksaraiva <patrick_saraiva@live.com>
2014-01-21 22:12:21 +0000

    prototipov1

---
+++ Prototipo/src/prototipo/Prototipo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prototipo;
+
+/**
+ *
+ * @author xavi
+ */
+public class Prototipo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
